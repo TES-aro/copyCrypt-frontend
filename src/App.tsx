13ae64c3249560728c5e4bc2e5bd34c8e071ff82
TES <taaviaro@student.uef.fi>
2026-01-13 01:10:14 +0200

    dabling in many pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link,
+} from "react-router-dom";
+import Home from "./pages/home";
+import TestPage from "./pages/TestPage";
 
 function App() {
   return (
     <>
-      <h1>hello world</h1>
+    <Router>
+    		<div>
+	    		<h1>hello world</h1>
+	    		<Link to="/">home</Link>
+	    		<Link to="/test">testPage</Link>
+	    	</div>
+
+	    	<div>
+    			<Routes>
+    				<Route path="/" element={<Home />} />
+    				<Route path="/test" element={<TestPage />} />
+    			</Routes>
+    		</div>
+    	</Router>
     </>
   )
 }
---
+++ src/pages/TestPage.tsx
@@ -0,0 +1,9 @@
+function TestPage(){
+	return(
+		<div>
+			<h3> this is a testPage </h3>
+		</div>
+	)
+}
+
+export default TestPage;
